025dbf5ca2b50225f8106c4d10b89b823994c092
sankamille <luc.brulet@epitech.eu>
2018-11-30 00:14:08 +0100

    [+] GET:POST:DELETE:UPDATE:FINDONE added and done

--- routes/usersHandler.go
+++ routes/usersHandler.go
@@ -1,30 +1,98 @@
 package routes
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/lbrulet/web-app-golang/config"
+
+	"github.com/lbrulet/web-app-golang/models"
+	"gopkg.in/mgo.v2/bson"
 )
+
+var db = dao.UsersDAO{}
 
 func YourHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("GOLANG API BY @https://github.com/lbrulet!\n"))
 }
 
 func AllUsers(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "not implemented yet !")
+	users, err := db.FindAll()
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJson(w, http.StatusOK, users)
 }
 
 func FindUser(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "not implemented yet !")
+	params := mux.Vars(r)
+	user, err := db.FindById(params["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid user ID")
+		return
+	}
+	respondWithJson(w, http.StatusOK, user)
 }
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "not implemented yet !")
+	defer r.Body.Close()
+	var user models.User
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	user.ID = bson.NewObjectId()
+	if err := db.Insert(user); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJson(w, http.StatusCreated, user)
 }
 
 func UpdateUser(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "not implemented yet !")
+	defer r.Body.Close()
+	var user models.User
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	if err := db.Update(user); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJson(w, http.StatusOK, map[string]string{"result": "success"})
 }
 
 func DeleteUser(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "not implemented yet !")
+	defer r.Body.Close()
+	var user models.User
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	if err := db.Delete(user); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJson(w, http.StatusOK, map[string]string{"result": "success"})
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	respondWithJson(w, code, map[string]string{"error": msg})
+}
+
+func respondWithJson(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
+func init() {
+
+	db.Server = "localhost"
+	db.Database = "users_db"
+	db.Connect()
 }
